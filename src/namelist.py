59d9081baccebb3c32a1b98d60495c6252beffe7
Evan <68309693+Iphisxu@users.noreply.github.com>
2023-07-13 21:14:54 +0800

    萧山2209分析

--- src/namelist.py
+++ src/namelist.py
@@ -1,26 +1,28 @@
 
 # basic information
-datadir = 'F:/Data/Project_Xiaoshan/202209/'
+#! the path changed when using different computer
+# progdir = 'F:/Data/Project_Xiaoshan/' #! YF-G15
+progdir = 'D:/data/Project_Xiaoshan/' #! A275-Desktop
+datadir = progdir + '202209/'
 runID   = 'XS_2209'
 
 # simulation data file
-gridfile = 'F:/Data/Project_Xiaoshan/GRIDCRO2D_2022234.nc'
+# gridfile = progdir + 'GRIDCRO2D_2022234.nc' #! YF-G15
+gridfile = progdir + 'GRIDCRO2D_2022234.nc' #! A275-Desktop
 mcipfile = datadir + runID + '_mcip.nc'
 cmaqfile = datadir + runID + '_chem.nc'
 pafile   = datadir + runID + '_pa.nc'
 isamfile = datadir + runID + '_isam.nc'
 
 # observation data file
-obsall = datadir + 'obsdata/allsite.xlsx'
-obsurban = datadir + 'obsdata/urban.xlsx'
+obshz = datadir + 'obsdata/hz_avg_2209.xlsx'
+obs6city = datadir + 'obsdata/6city_avg_2209.xlsx'
 
 # shapefile
-shpall   = datadir + 'shapefile/Anqing/Anqing.shp'
-shpurban = datadir + 'shapefile/Anqing_urban/urban.shp'
-shprural = datadir + 'shapefile/Anqing_rural/rural.shp'
-shpmap   = datadir + 'shapefile/Anqing_district/anqing_district.shp'
+shphz   = progdir + 'shapefile/杭州市/杭州市.shp'
+shpxs = progdir + 'shapefile/萧山区/萧山区.shp'
+shpmap   = progdir + 'shapefile/杭州市各区/杭州市各区.shp'
 
 # time range
 timestart = '2022-09-01T00'
 timeend   = '2022-09-30T23'
-
